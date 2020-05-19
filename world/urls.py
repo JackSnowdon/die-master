e49627c5fb1ae7d8398addb40c80f52c5291a0a1
JackSnowdon <jacksnowdondrums@gmail.com>
2020-05-19 18:56:31 +0100

    Crud functions fully up and running

--- world/urls.py
+++ world/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('world_home/', world_home, name="world_home"),
-    path('add_dark_heresy/', add_dark_heresy, name="add_dark_heresy")
+    path('add_dark_heresy/', add_dark_heresy, name="add_dark_heresy"),
+    path(r'edit_dark_heresy/<int:pk>', edit_dark_heresy, name="edit_dark_heresy"),
+    path(r'delete_dark_heresy/<int:pk>', delete_dark_heresy, name="delete_dark_heresy"),
+    path(r'single_dark_heresy/<int:pk>', single_dark_heresy, name="single_dark_heresy"),
 ]
--- world/views.py
+++ world/views.py
@@ -27,3 +27,59 @@ def add_dark_heresy(request):
     else:
         dark_form = DarkBaseForm()
     return render(request, "add_dark_heresy.html", {"dark_form": dark_form}) 
+
+
+@login_required
+def edit_dark_heresy(request, pk):
+    this_sheet = get_object_or_404(DarkHeresyBase, pk=pk)
+    profile = request.user.profile
+    if profile == this_sheet.created_by or profile.staff_access:
+        if request.method == "POST":
+            dark_form = DarkBaseForm(request.POST, instance=this_sheet)
+            if dark_form.is_valid():
+                form = dark_form.save(commit=False)
+                form.save()
+                messages.error(
+                    request, "Edited {0}".format(form.name), extra_tags="alert"
+                )
+                return redirect("world_home")
+        else:
+            dark_form = DarkBaseForm(instance=this_sheet)
+        return render(
+            request,
+            "edit_dark_heresy.html",
+            {"dark_form": dark_form, "this_sheet": this_sheet},
+        )
+    else:
+        messages.error(
+            request, "You Don't Have The Required Permissions", extra_tags="alert"
+        )
+        return redirect("world_home")
+
+
+@login_required
+def delete_dark_heresy(request, pk):
+    this_sheet = get_object_or_404(DarkHeresyBase, pk=pk)
+    profile = request.user.profile
+    if profile == this_sheet.created_by or profile.staff_access:
+        messages.error(request, 'Deleted {0}'.format(this_sheet.name), extra_tags='alert')
+        this_sheet.delete()
+        return redirect(reverse('world_home'))
+    else:
+        messages.error(
+            request, "You Don't Have The Required Permissions", extra_tags="alert"
+        )
+        return redirect("world_home") 
+
+
+@login_required
+def single_dark_heresy(request, pk):
+    this_sheet = get_object_or_404(DarkHeresyBase, pk=pk)
+    profile = request.user.profile
+    if profile == this_sheet.created_by or profile.staff_access:
+        return render(request, "single_dark_heresy.html", {"this_sheet": this_sheet})
+    else:
+        messages.error(
+            request, "You Don't Have The Required Permissions", extra_tags="alert"
+        )
+        return redirect("world_home") 
