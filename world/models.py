d38e29cb6aac841d53b9c26220aa1d39a1d33aad
JackSnowdon <jacksnowdondrums@gmail.com>
2020-05-26 14:42:31 +0100

    Changed on_delete within base models to set_null to allow deletion of games

--- world/models.py
+++ world/models.py
@@ -19,9 +19,9 @@ class DarkHeresyBase(models.Model):
     influence = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)], default=0)
     max_fate_points = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)], default=0)
     created_by = models.ForeignKey(Profile, related_name='dh_sheets', on_delete=models.PROTECT)
-    current_game = models.ForeignKey(DarkHeresyGame, related_name='sheets', on_delete=models.PROTECT, blank=True,
+    current_game = models.ForeignKey(DarkHeresyGame, related_name='sheets', on_delete=models.SET_NULL, blank=True,
         null=True)
-    die_roll = models.ForeignKey(DarkDieRoll, related_name='die_rolls', on_delete=models.PROTECT, blank=True,
+    die_roll = models.ForeignKey(DarkDieRoll, related_name='die_rolls', on_delete=models.SET_NULL, blank=True,
         null=True)
 
     def __str__(self):
